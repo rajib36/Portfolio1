6d0d5a3eee6db292c422b092f2de0651cbe7acd7
rajib36 <93149302+rajib36@users.noreply.github.com>
2021-10-29 12:27:46 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+/*------------Start Skill JS-------------*/
+var object = [
+
+{
+
+                    'headline':'HTML & CSS',
+                    'value':8,
+                    'length':10,
+                    'description':'Significant experience and knowlage of HTML(5) and CSS functionality and use.'
+
+                },
+                {
+
+                    'headline':'JavaScript & jQuery',
+                    'value':5,
+                    'length':10,
+                    'description':'Experience with object-oriented JavaScript. </br> Extended knowlage of DOM manipulation in aiding and extending the UI.'
+
+                },
+                {
+
+                    'headline':'Bootstrap',
+                    'value':7,
+                    'length':10,
+                    'description':'Experience with object-oriented JavaScript. </br> Extended knowlage of DOM manipulation in aiding and extending the UI.'
+
+                },
+                {
+
+                    'headline':'PHP',
+                    'value':4,
+                    'length':10,
+                    'description':'Experience with object-oriented JavaScript. </br> Extended knowlage of DOM manipulation in aiding and extending the UI.'
+
+                },
+                {
+
+                    'headline':'Wordpress',
+                    'value':6,
+                    'length':10,
+                    'description':'Experience with object-oriented JavaScript. </br> Extended knowlage of DOM manipulation in aiding and extending the UI.'
+
+                }
+
+];
+
+$(document).ready(function(){
+
+$("#skillset").skillset({
+
+object:object,
+duration:70
+
+});
+
+});
+/*--------------End Skills JS--------------*/
